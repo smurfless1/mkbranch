31c7cef9ceac6d853956077d16667fd55142fd18
David Brown <business@smurfless.com>
2022-06-11 09:40:27 -0500

    Add tasks with secure password storage

---
+++ tasks.py
@@ -0,0 +1,37 @@
+import json
+
+from invoke import task
+
+@task
+def publish(c):
+    token = get_field(c, "Github", "api token for goreleaser").strip()
+    c.run(f'GITHUB_TOKEN="{token}" goreleaser release')
+
+@task
+def build_only_local_platform(c):
+    c.run("goreleaser build --single-target --rm-dist")
+
+
+def get_field(c, name, field):
+    c: Context
+    result = c.run(f'op item get "{name}" --field label="{field}"', hide='both')
+    return result.stdout
+
+
+def get_username(c, name):
+    c: Context
+    result = c.run(f'op item get "{name}" --field label="username"', hide='both')
+    return result.stdout
+
+
+def get_password(c, name):
+    c: Context
+    result = c.run(f'op item get "{name}" --field label="password"', hide='both')
+    return result.stdout
+
+
+def get_otp(c, name):
+    c: Context
+    result = c.run(f'op item get "{name}" --otp', hide='both')
+    return result.stdout
+
